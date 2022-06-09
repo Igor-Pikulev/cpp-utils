860bc26fc8e3b03c02f462b115d73fd514c537bf
Igor <60849427+Igor-Pikulev@users.noreply.github.com>
2022-06-09 22:36:54 +0300

    Update inttobin.cpp

--- inttobin.cpp
+++ inttobin.cpp
@@ -6,13 +6,13 @@ void intToBin (int n) {
 	
 	if (n>1) 
         intToBin(n/2);
-    cout<<n%2;
+    cout << n%2;
 }
 
 int main() {
     
     int intNum;
-    cin >>intNum;
+    cin >> intNum;
     intToBin(intNum);
 
     return 0;
